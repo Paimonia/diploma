53986c1d24e4d4e09fe8475a0f026ddf13e7cc17
Paimonia <aflorko@logstream.ru>
2023-05-05 00:12:46 +0300

    Доработка экрана тренировок

--- Frontend/src/views/trainingexecute.js
+++ Frontend/src/views/trainingexecute.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import withRouter from '../shared/withrouter'
 import Grid from '@mui/material/Grid'
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player'
+import Button from '@mui/material/Button'
 
 import AuthService from '../services/auth.js'
 import DataService from '../services/data.js'
@@ -14,7 +15,10 @@ class TrainingExecute extends React.Component {
             training: {},
             exercises: undefined,
             currenttrainingexercise: undefined,
-            currentexercise: undefined             
+            currentexercise: undefined,
+            nextexercise: undefined,
+            repeat_count: 1,
+            isResting: false         
         }
         
         this.trainingId =  this.props.params.id 
@@ -33,10 +37,12 @@ class TrainingExecute extends React.Component {
             this.setState({ exercises: values[0].data }, () => {
                 this.setState({ training: values[1].data }, () => {
                     alert(JSON.stringify(this.state))
-                    const fte = this.state.training.exercises[0]   
+                    const fte = this.state.training.exercises.length >= 0 ? this.state.training.exercises[0] : undefined  
+                    const nte = this.state.training.exercises.length >= 1 ? this.state.training.exercises[1] : undefined 
                     this.setState({
                         currenttrainingexercise : fte,
-                        currentexercise : this.state.exercises.find(e => e.id === fte.exercise_id) 
+                        currentexercise : this.state.exercises.find(e => e.id === fte?.exercise_id),
+                        nextexercise : this.state.exercises.find(e => e.id === nte?.exercise_id),                         
                     })                
                 })
             })
@@ -48,11 +54,14 @@ class TrainingExecute extends React.Component {
     render() {
         return (
             <div>
-                <h1>Тренировка {this.state.training.name}</h1>            
-                <Grid container>                    
-                    <Grid item xs="4">
+                  <Grid container>                    
+                    <Grid item xs="4">                        
                         <Grid container>
                             <Grid container>
+                                <Grid item xs="12">
+                                    <h1>Тренировка {this.state.training.name}</h1>  
+                                    <h2>Повтор: {this.state.repeat_count} / {this.state.training.repeat_count}</h2>                       
+                                </Grid>
                                 <Grid item xs="4">
                                     <h3>Упражнение</h3>
                                 </Grid>
@@ -64,20 +73,29 @@ class TrainingExecute extends React.Component {
                                 </Grid>                                
                             </Grid>
                         {this.state.training?.exercises?.map(e => {
-                            const ex = this.state.exercises.find(z => e.exercise_id === z.id)                               
+                            const ex = this.state.exercises.find(z => e.exercise_id === z.id)
+                            const exerciseRowStyle = this.state.isResting ? undefined : { backgroundColor: e.id === this.state.currenttrainingexercise?.id ? '#1976d2' : '' }
+                            const restRowStyle = !this.state.isResting ? undefined :  { backgroundColor: e.id === this.state.currenttrainingexercise?.id ? '#1976d2' : '' }                            
                             return (
-                            <Grid container style={{ backgroundColor: e.id === this.state.currenttrainingexercise?.id ? '#1976d2' : '' }}>
-                                <Grid item xs="6">
+                            <Grid container>
+                                <Grid item xs="6" style={exerciseRowStyle}>
                                     {ex?.title}
                                 </Grid>
-                                <Grid item xs="3">
+                                <Grid item xs="3" style={exerciseRowStyle}>
                                     {e.worked_time} 
                                 </Grid>
-                                <Grid item xs="3">
+                                <Grid item xs="3" style={exerciseRowStyle}>
                                     {e.work_time} 
                                 </Grid>
+                                <Grid item xs="6" style={restRowStyle}>
+                                    Отдых
+                                </Grid>
+                                <Grid item xs="3" style={restRowStyle}>
+                                </Grid>
+                                <Grid item xs="3" style={restRowStyle}>
+                                    {e.completion_rest_time} 
+                                </Grid>
                             </Grid>
-                            
                             )
                         })}
                         </Grid>
@@ -87,15 +105,32 @@ class TrainingExecute extends React.Component {
                     {this.state.currenttrainingexercise && <Grid xs="7">
                         <h2>{this.state.currentexercise?.title}</h2>
                         <p>{this.state.currentexercise?.description}</p>
-                        <h2>Осталось {this.state.currenttrainingexercise?.work_time - this.state.currenttrainingexercise?.worked_time} сек.</h2>                        
-                        <ReactPlayer 
-                                    width='100%' 
-                                    loop='true'
-                                    playing='true'
-                                    playsinline='true' 
-                                    url={this.state.currentexercise? `http://localhost:4000/videos/exercises/${this.state.currentexercise.id}.mp4` : ''} 
-                        />
+                        <Grid container>
+                            <Grid item xs="8">
+                                <h2>Осталось {this.state.currenttrainingexercise?.work_time - this.state.currenttrainingexercise?.worked_time} сек.</h2>                                                
+                                <ReactPlayer 
+                                            width='90%' 
+                                            loop='true'
+                                            playing='true'
+                                            playsinline='true' 
+                                            url={this.state.currentexercise? `http://localhost:4000/videos/exercises/${this.state.currentexercise.id}.mp4` : ''} 
+                                />
+                            </Grid>
+                            <Grid item xs="4">
+                                <h2>Следующее упражнение</h2>
+                                <ReactPlayer 
+                                            width='90%' 
+                                            playsinline='false' 
+                                            url={this.state.nextexercise? `http://localhost:4000/videos/exercises/${this.state.nextexercise.id}.mp4` : ''} 
+                                />
+                            </Grid>
+                        </Grid>
                     </Grid>}
+                </Grid>
+                <Grid container>
+                    <Button variant="contained">СТАРТ</Button>&nbsp;
+                    <Button variant="contained">СТОП</Button>&nbsp;
+                    <Button variant="contained">ПАУЗА</Button>                    
                 </Grid>
             </div>           
         )
